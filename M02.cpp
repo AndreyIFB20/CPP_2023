721baee2d0974b2ff53ff44d1be356e49b456bb7
AndreyIFB20 <125687214+AndreyIFB20@users.noreply.github.com>
2023-03-17 16:13:23 +0800

    Update M02.cpp

--- M02.cpp
+++ M02.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+int ploshad(float A, int N)
+{
+float S;
+const double pi = 3.141592653589793;
+if (N == 3)
+{
+    S = sqrt(3) / 4 * A * A;
+    cout << "Площадь многоугольника равна: " << S;
+
+}
+else
+{
+    S = (A * A * N) / (4 * tan(pi/N));
+    cout << "Площадь многоугольника равна: " << S;
+}
+return S;
+}
 int main()
 {
-setlocale(LC_ALL, "Russian");
+setlocale(0, "RUSSIAN");
+float A;
 int N;
-float A, S;
-const double pi = 3.141592653589793;
 cout << "Введите длину стороны: ";
 cin >> A;
 while (A < 0)
@@ -21,16 +37,5 @@ while (N < 3)
     cout << endl << "Введите число сторон: ";
     cin >> N;
 }
-if (N == 3)
-{
-    S = sqrt(3) / 4 * A * A;
-    cout << "Площадь многоугольника равна: " << S;
-
-}
-else
-{
-    S = (A * A * N) / (4 * tan(pi/N));
-    cout << "Площадь многоугольника равна: " << S;
-}
-return 0;
+ploshad(A, N);
 }
